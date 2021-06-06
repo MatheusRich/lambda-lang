29bde8831291d673befac58c53c7adafb3097c16
Matheus Richard <matheusrichardt@gmail.com>
2021-06-06 17:45:58 -0300

    Update main

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,18 @@ use parser::Parser;
 use token::Token;
 use token_stream::TokenStream;
 
-fn main() {}
+fn main() {
+    use std::io::{stdin, stdout, Write};
+
+    print!("> ");
+    let _ = stdout().flush();
+
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("Invalid string");
+    let mut parser = Parser::new(TokenStream::new(InputStream::new(input)));
+
+    println!("Parsed successfully: {:?}", parser.parse());
+}
 
 #[cfg(test)]
 mod tests {
@@ -23,6 +34,15 @@ mod tests {
 
     fn assert_vec_eq(va: &[Expression], vb: &[Expression]) {
         assert!((va.len() == vb.len()) && va.iter().zip(vb).all(|(a, b)| a == b))
+    }
+
+    fn literal(kind: &str, value: &str) -> Box<Expression> {
+        match kind {
+            "num" => Box::new(Expression::Num {
+                value: value.parse().expect("Invalid float"),
+            }),
+            _ => panic!("Don't know how to create literal {}", kind),
+        }
     }
 
     #[test]
