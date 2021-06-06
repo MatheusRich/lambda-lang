6b226b7c2e0afb3322fe29e0210ee21814c58805
Matheus Richard <matheusrichardt@gmail.com>
2021-06-06 17:25:44 -0300

    expect > unwrap

--- src/token_stream.rs
+++ src/token_stream.rs
@@ -135,7 +135,7 @@ impl TokenStream {
         }
 
         Token::Num {
-            value: number.parse().unwrap(),
+            value: number.parse().expect("Couldn't parse number"),
         }
     }
 
