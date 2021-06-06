fdf6e348edf534894e74de385403de54dc0deb9b
Matheus Richard <matheusrichardt@gmail.com>
2021-06-06 15:27:22 -0300

    Add more tokens

--- src/token.rs
+++ src/token.rs
@@ -1,10 +1,28 @@
-#[derive(Clone)]
+use std::fmt;
+
+#[derive(Clone, Debug)]
 pub enum Token {
-    Punc { value: String },
-    Num { value: f64 },
-    Str { value: String },
     Kw { value: String },
-    Var { value: String },
+    Num { value: f64 },
     Op { value: String },
+    Punc { value: String },
+    Str { value: String },
+    Var { value: String },
     Error,
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let result: String = match self {
+            Token::Kw { value }
+            | Token::Op { value }
+            | Token::Punc { value }
+            | Token::Str { value }
+            | Token::Var { value } => value.to_string(),
+            Token::Num { value } => value.to_string(),
+            _ => String::from("ERROR"),
+        };
+
+        write!(f, "{}", result)
+    }
 }
